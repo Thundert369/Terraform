f094f81a427be2108c9e87114a4a33502b2a6d54
Thundert369 <149377120+Thundert369@users.noreply.github.com>
2023-12-11 13:13:56 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -17,13 +17,3 @@ provider "azurerm" {
     tenant_id       = var.tenant_id    
     features {}
 }
-
-//azread = {
-  //      source  = "hashicorp/azuread"
-    //  version = "~> 2.15.0"
-    //}
-//provider "azread" { 
-  //  client_id   = "feba6224-9e9f-4226-96e7-0363f5f38bcd"
-//client_secret   = "6583906a-346d-4d3f-970c-e227b5ede102"
-//tenant_id       = "9209acaf-010d-4db9-a033-4d38d8290d92"
-//}
